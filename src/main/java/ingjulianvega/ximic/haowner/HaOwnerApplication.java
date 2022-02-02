428397bf00f33fe9df36dcf04cb56013a986d2eb
Julian Camilo Vega Rocha <julian.vega@globant.com>
2022-02-02 16:06:10 -0500

    ✨ Initial commit basic pom configuration

---
+++ src/main/java/ingjulianvega/ximic/haowner/HaOwnerApplication.java
@@ -0,0 +1,13 @@
+package ingjulianvega.ximic.haowner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaOwnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaOwnerApplication.class, args);
+	}
+
+}
