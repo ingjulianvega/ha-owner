fe89f6dd824974befa4c41419fb26ea40bda0a85
Julian Camilo Vega Rocha <julian.vega@globant.com>
2022-02-06 17:38:25 -0500

    ✨ new endpoint to query a list of pets given a personId,

--- src/main/java/ingjulianvega/ximic/haowner/configuration/LocalDiscovery.java
+++ src/main/java/ingjulianvega/ximic/haowner/configuration/LocalDiscovery.java
@@ -1,8 +1,10 @@
 package ingjulianvega.ximic.haowner.configuration;
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+@Configuration
 @Profile("local-discovery")
 @EnableDiscoveryClient
 public class LocalDiscovery {
